9c9c094f92e6ecc8a7702da08b8019071b583db5
Jumps710 <nonattonolife@gmail.com>
2024-09-08 08:23:08 +0900

    Update script.js

--- script.js
+++ script.js
@@ -51,7 +51,7 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 
     function sendData(params) {
-        const GAS_WEB_APP_URL = 'https://script.google.com/macros/s/YOUR_DEPLOYMENT_ID/exec';
+        const GAS_WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbwIdZiP3KB3Tf6wMegdXXcorGE6E-djR3rewZLbBI2QBZa_VHYUrODRpdkO8jIhLvnD/exec';
 
         fetch(GAS_WEB_APP_URL, {
             method: 'POST',
